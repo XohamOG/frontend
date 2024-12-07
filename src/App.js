4361b307963bd2a6bd77d39d24b5ac16fa06b619
Soham <141822047+XohamOG@users.noreply.github.com>
2024-12-07 18:52:09 +0530

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import HomePage from "./components/HomePage";
 import AddPlayerForm from "./components/AddPlayerForm";
 import ApiComponent from "./ApiComponent";
 import TeamFormation from "./TeamFormation";
-import "./App.css"; // Include the CSS
 
 function App() {
   return (
