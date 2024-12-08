68316b1282c905a2b7f8c493eab1e5051d0c1e11
Soham <141822047+XohamOG@users.noreply.github.com>
2024-12-08 15:06:00 +0530

    better team generetion UI

--- src/components/TeamList.js
+++ src/components/TeamList.js
@@ -1,22 +1,26 @@
-// src/components/TeamList.js
 import React from 'react';
+import '../styles/TeamList.css'; // Ensure to import the updated styles
 
 const TeamList = ({ teams }) => {
   return (
-    <div className="teams">
+    <div className="team-container">
       <div className="team">
-        <h3>Team 1</h3>
+        <h3 className="team-header">Team 1</h3>
         <ul>
           {teams.team1.map((player, index) => (
-            <li key={index}>{player}</li>
+            <li key={index} className="player">
+              {player}
+            </li>
           ))}
         </ul>
       </div>
       <div className="team">
-        <h3>Team 2</h3>
+        <h3 className="team-header">Team 2</h3>
         <ul>
           {teams.team2.map((player, index) => (
-            <li key={index}>{player}</li>
+            <li key={index} className="player">
+              {player}
+            </li>
           ))}
         </ul>
       </div>
